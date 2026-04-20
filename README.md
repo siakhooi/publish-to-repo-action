9927a2c651b6681257ce6b3304d0954fe42b361b
Siak Hooi <siakhooi@gmail.com>
2026-04-20 04:48:05 +0000

    initial

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # publish-to-repo-action
-github action to publish files to another repo
+github action to publish files to another github repo
+
+## Sample publish-to-repo.yaml
+
+```
+repo: username/yyyyy
+branch: main
+git_commit_user: xxxxxx
+git_commit_email: xxxxxx@example.com
+git_commit_message: "xxxxxx: Auto deploy"
+
+files:
+  - source: /path/in/source
+    target: path/in/target
+```
+
+## Usage
+### Github Workflow File
+```
+name: Publish Files
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+  workflow_dispatch:
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - name: Publish Files
+        uses: siakhooi/publish-to-repo-action@main
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+```
+### `publish-to-repo.yml`
+```
+repo: username/yyyyy
+branch: main
+git_commit_user: xxxxxx
+git_commit_email: xxxxxx@example.com
+git_commit_message: "xxxxxx: Auto deploy"
+
+files:
+  - source: /path/in/source
+    target: path/in/target
+```
---
+++ publish-to-repo.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+readonly configFile=$1
+readonly token=$2
+
+configFilePath=$(realpath "${GITHUB_WORKSPACE}/$configFile")
+
+if [[ ! -f "$configFilePath" ]]; then
+	echo "Config file not found: $configFilePath"
+	exit 1
+fi
+
+if [[ -z "$token" ]]; then
+	echo "Token is required"
+	exit 1
+fi
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+	echo "GITHUB_WORKSPACE is not set"
+	exit 1
+fi
+if command -v git &>/dev/null; then
+	echo "Git is installed"
+else
+	echo "Git is not installed."
+	exit 1
+fi
+if command -v yq &>/dev/null; then
+	echo "yq is installed"
+else
+	echo "yq is not installed."
+	exit 1
+fi
+
+set -euo pipefail
+
+repoBranch=$(yq e '.branch' "$configFilePath")
+repo=$(yq e '.repo' "$configFilePath")
+repoUrl="https://x-access-token:${token}@github.com/${repo}.git"
+gitCommitEmail=$(yq e '."git_commit_email"' "$configFilePath")
+gitCommitUser=$(yq e '."git_commit_user"' "$configFilePath")
+gitCommitMessage=$(yq e '."git_commit_message"' "$configFilePath")
+
+fullCommitMessage="$gitCommitMessage | ${GITHUB_SHA:0:7} | $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+git config user.email "$gitCommitEmail"
+git config user.name "$gitCommitUser"
+
+# Check if .files array is empty or missing
+files_count=$(yq e '.files | length' "$configFilePath")
+if [[ -z "$files_count" || "$files_count" -eq 0 ]]; then
+	echo ".files array is empty or missing in config file: $configFilePath"
+	exit 1
+fi
+
+workingDirectory=$(mktemp -d)
+
+git clone --depth=1 --branch "$repoBranch" "$repoUrl" "$workingDirectory"
+
+cd "$workingDirectory" || {
+	echo "Failed to change directory to $workingDirectory"
+	exit 1
+}
+
+yq -r '.files[] | "\(.source) \(.target)"' "$configFilePath" | while read -r sourcePath targetPath; do
+	echo "Source: $sourcePath, Target: $targetPath"
+
+	# source path always relative to $GITHUB_WORKSPACE, /* means relative to $GITHUB_WORKSPACE
+	srcPath=$(realpath "${GITHUB_WORKSPACE}/$sourcePath")
+
+	# target path always relative to repo root, /* means relative to repo root
+	tgtPath=$(realpath "$PWD/$targetPath")
+
+	echo "Source: $srcPath"
+	echo "Target: $tgtPath"
+
+	if [[ -f "$srcPath" ]]; then
+		mkdir -p "$(dirname "$tgtPath")"
+		finalTargetPath="$tgtPath/$(basename "$srcPath")"
+		echo "Source is a file, copying to $finalTargetPath"
+		cp -v "$srcPath" "$finalTargetPath"
+
+	elif [[ -d "$srcPath" ]]; then
+		echo "Source is a directory, copying contents"
+		mkdir -p "$tgtPath"
+		cp -rv "$srcPath/." "$tgtPath/"
+
+	else
+		echo "Source path does not exist: $srcPath"
+		exit 1
+	fi
+done
+
+git add --all
+git status
+if git diff --cached --quiet; then
+	echo "No changes to commit"
+	exit 1
+fi
+git remote set-url origin "$repoUrl"
+git commit -m "$fullCommitMessage"
+git push origin "$repoBranch"
